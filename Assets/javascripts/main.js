d6bd6b68b2d011d7af25d7441c3251d18e51aed9
kgdiem <kg.diem@gmail.com>
2016-03-19 18:13:11 -0400

    pi chart included/needs styled/touched up

--- Assets/javascripts/main.js
+++ Assets/javascripts/main.js
@@ -8,8 +8,6 @@ $(document).ready(function() {
     firebase.on("value", function (snapshot) {
         //assign the data from firebase to a  variable
         firebaseData = snapshot.val();
-        console.log(formatDateChart(firebaseData));
-        console.log(firebaseData);
 
         var chart = AmCharts.makeChart("chartdiv", {
             "type": "serial",
@@ -94,13 +92,71 @@ $(document).ready(function() {
         function zoomChart() {
             chart.zoomToIndexes(chart.dataProvider.length - 40, chart.dataProvider.length - 1);
         }
+
+        var pichart = AmCharts.makeChart("pichart", {
+            "type": "pie",
+            "startDuration": 0,
+            "theme": "light",
+            "addClassNames": true,
+            "legend":{
+                "position":"right",
+                "marginRight":100,
+                "autoMargins":false
+            },
+            "innerRadius": "30%",
+            "defs": {
+                "filter": [{
+                    "id": "shadow",
+                    "width": "200%",
+                    "height": "200%",
+                    "feOffset": {
+                        "result": "offOut",
+                        "in": "SourceAlpha",
+                        "dx": 0,
+                        "dy": 0
+                    },
+                    "feGaussianBlur": {
+                        "result": "blurOut",
+                        "in": "offOut",
+                        "stdDeviation": 5
+                    },
+                    "feBlend": {
+                        "in": "SourceGraphic",
+                        "in2": "blurOut",
+                        "mode": "normal"
+                    }
+                }]
+            },
+            "dataProvider": formatSexChart(firebaseData),
+            "valueField": "Incarcerated",
+            "titleField": "Gender",
+            "export": {
+                "enabled": true
+            }
+        });
+
+        pichart.addListener("init", handleInit);
+
+        pichart.addListener("rollOverSlice", function(e) {
+            handleRollOver(e);
+        });
+
+        function handleInit(){
+            pichart.legend.addListener("rollOverItem", handleRollOver);
+        }
+
+        function handleRollOver(e){
+            var wedge = e.dataItem.wedge.node;
+            wedge.parentNode.appendChild(wedge);
+        }
+
     });
 
 });
 
 function formatDateChart(data){
     //type checking
-    if(data === "undefined"){
+    if(!data){
         return "error, data is undefined";
     }
     else if(typeof(data) !== "object"){
@@ -120,6 +176,37 @@ function formatDateChart(data){
         }
         //return array
         return formatted;
+    }
+}
+
+function formatSexChart(data){
+    if(!data){
+        return "error, data is undefined";
+    }
+    else if(typeof(data) !== "object"){
+        return "error, argument should be an object";
+    }
+    else{
+        //instantiate total males and females for increment
+        var totalMale = 0, totalFemale = 0, total = 0, avgM = 0, avgF = 0;
+        for(var prop in data){
+            console.log(data[prop]["Total "]["Adult Male"]);
+            totalMale += parseInt(data[prop]["Total "]["Adult Male"]);
+            totalFemale += parseInt(data[prop]["Total "]["Adult Female"]);
+            total++;
+        }
+        avgM = totalMale/total;
+        avgF = totalFemale/total;
+        return [
+            {
+                "Gender": "Male",
+                "Incarcerated": Math.round(avgM)
+            },
+            {
+                "Gender": "Female",
+                "Incarcerated": Math.round(avgF)
+            }
+        ]
     }
 }
 
